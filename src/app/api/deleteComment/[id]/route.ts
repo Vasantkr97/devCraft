507e148b59c00686624f2b9217ef136c4170e5e0
Vasantkr97 <vasantkr97@gmail.com>
2025-02-10 13:34:55 +0530

    fi

--- src/app/api/deleteComment/[id]/route.ts
+++ src/app/api/deleteComment/[id]/route.ts
@@ -10,15 +10,17 @@ export async function DELETE(req: NextRequest, {params}: {params: { id: string }
         const clerkUser = await currentUser();
         const user = clerkUser ? await getUser(clerkUser?.id) : null;
 
-        if (!user) throw new Error("Not authenticated!")
-
+        if (!user) {
+            return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+        }
+        
         if (!commentId) {
             return NextResponse.json({ error: "Comment ID is required"}, { status: 400 })
         }
 
         await deleteComment(commentId);
 
-        return NextResponse.json({ status: 200 });
+        return NextResponse.json({ message: "Comment deleted successfully" }, { status: 200 });
     } catch (error) {
         console.log("Error deleting Comment:", error);
         return NextResponse.json({ error: "Internal server error"}, { status: 500 })
