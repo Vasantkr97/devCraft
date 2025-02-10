d8c85ab93f7834a51e06f669cca477922da2116e
Vasantkr97 <vasantkr97@gmail.com>
2025-02-10 12:31:05 +0530

    Fixed

--- src/app/api/addComment/[id]/route.ts
+++ src/app/api/addComment/[id]/route.ts
@@ -1,23 +1,20 @@
 import { addComment } from "@/db/snippet";
 import { NextRequest, NextResponse } from "next/server";
 
-
-export async function POST(req: NextRequest, res: NextResponse) {
-    
+export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
-        const { snippetId, content }= body;
+        const { snippetId, content } = body;
 
         if (!snippetId || !content?.trim()) {
-            return NextResponse.json({ error: "INvalid input"}, { status: 400 });
+            return NextResponse.json({ error: "Invalid input" }, { status: 400 });
         }
 
         const comment = await addComment(snippetId, content);
-        
         return NextResponse.json(comment, { status: 201 });
 
-    } catch(error) {
-        console.log("Error adding Comment", error);
-        return NextResponse.json({ error: "internal server error"}, { status: 500 });
+    } catch (error) {
+        console.error("Error adding comment:", error);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
     }
 }
